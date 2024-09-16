6b09a904c927085f125c3bc9ae0ef2b63ed66c2b
Pedro de Castro Kurtz <131386211+nanic1@users.noreply.github.com>
2024-09-16 11:03:32 -0300

    Add files via upload

---
+++ Aula dia 16.09.2024/exercicio1.js
@@ -0,0 +1,21 @@
+function func1(x, y){
+    return x + y;
+} 
+
+const func2 = function(a, b){
+    return a * b;
+}
+
+const func3 = () => {
+    return "bonito";
+}
+
+const minha_lista = [25, "casa"];
+
+funcaocomnome = (elemento) => {console.log(elemento)}
+minha_lista.forEach(funcaocomnome);
+var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+function tabuadaDe2(item) {
+    console.log(item*2);
+}
